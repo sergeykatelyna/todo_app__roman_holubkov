c9d4cbde7f7edd580a52c6fa74cdb280e56d5f5b
Ruslan Lukianenko <ruslan201010@gmail.com>
2020-09-02 20:28:53 +0300

    table push

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,51 @@
 import './assets/styles/main.scss';
+
+const users = [
+  {
+    active: true,
+    contact: {
+      phoneNumber: '380506535249',
+    },
+    createdAt: '2020-08-28T11:33:14.237Z',
+    id: '23cff83e-4ed4-43a5-ae63-a31310a1076a',
+    name: 'User1',
+  },
+  {
+    active: true,
+    contact: {
+      phoneNumber: '380506535249',
+    },
+    createdAt: '2020-08-28T11:33:14.237Z',
+    id: '23cff83e-4ed4-43a5-ae63-a31310a1076a',
+    name: 'User2',
+  },
+  {
+    active: true,
+    contact: {
+      phoneNumber: '380506535249',
+    },
+    createdAt: '2020-08-28T11:33:14.237Z',
+    id: '23cff83e-4ed4-43a5-ae63-a31310a1076a',
+    name: 'User3',
+  },
+  {
+    active: true,
+    contact: {
+      phoneNumber: '380506535249',
+    },
+    createdAt: '2020-08-28T11:33:14.237Z',
+    id: '23cff83e-4ed4-43a5-ae63-a31310a1076a',
+    name: 'User1',
+  },
+  {
+    active: true,
+    contact: {
+      phoneNumber: '380506535249',
+    },
+    createdAt: '2020-08-28T11:33:14.237Z',
+    id: '23cff83e-4ed4-43a5-ae63-a31310a1076a',
+    name: 'User4',
+  },
+];
+
+console.log(users);
